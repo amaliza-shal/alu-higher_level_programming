6be2ea5610fcb565d4eb06d15ca32e166867a52a
Amaliza-shal <s.amaliza@alustudent.com>
2024-11-08 04:56:01 -0800

    6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+
+#!/usr/bin/python3
+'''Public instance method: def area(self):'''
+
+
+class BaseGeometry:
+    '''Base class'''
+
+    def area(self):
+        '''raise an exception for now'''
+        raise Exception("area() is not implemented")
+
