a8d736f6bbf682d69c21ddea10c081ba76ed59ea
Amaliza-shal <s.amaliza@alustudent.com>
2024-11-08 06:06:48 -0800

    6

--- python-more_classes/6-rectangle.py
+++ python-more_classes/6-rectangle.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/python3
 """Define a rectangle"""
 
@@ -83,4 +82,3 @@ class Rectangle:
         to be able to recreate a new instance
         """
         return "Rectangle({}, {})".format(self.__width, self.__height)
-
