639dee802731c630cea456e8fd27053819fc431a
Amaliza-shal <s.amaliza@alustudent.com>
2024-11-14 01:17:58 -0800

    5

--- python-input_output/5-save_to_json_file.py
+++ python-input_output/5-save_to_json_file.py
@@ -1,13 +1,18 @@
-
 #!/usr/bin/python3
-'''A function that reads a json file'''
-
+# function that writes an Object to a text file,
+# using a JSON representation
+"""
+    define a function 'save_to_json_file'
+    import 'json'
+"""
 
 import json
 
 
 def save_to_json_file(my_obj, filename):
-    '''saves a doc to a json format'''
-    with open(filename, 'w+') as f:
-        return json.dump(my_obj, f)
+    """
+        writes an object to a text file, using json representation
+    """
 
+    with open(filename, 'w', encoding='utf-8') as f:
+        return json.dump(my_obj, f)
